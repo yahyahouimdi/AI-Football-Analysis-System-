0769f586036810890cb0f4439a761be0d5a210cc
yahya houimdi <yahya.houimdi@etudiant-enit.utm.tn>
2026-09-04 16:58:48 +0100

    fix

--- yolo_infernce.py
+++ yolo_infernce.py
@@ -6,7 +6,7 @@ video_path = Path("input_video") / "video.mp4"
 if not video_path.is_file():
     raise FileNotFoundError(f"Input video not found: {video_path.resolve()}")
 
-model = YOLO("yolov8s")  # load a pretrained YOLOv8n model
+model = YOLO("yolov8x") # load a pretrained YOLOv8n model
 results = model.predict(str(video_path), save=True)
 print(results[0])
 print('=================================')
